d45c3cdc3b71f762ca3e94562f3fbc9e8e329ad6
kjnwjn <kjnwjnpham@gmail.com>
2021-10-18 16:15:47 +0700

    done test

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-<<<<<<< HEAD
 var SearchbarHistory = document.querySelector('.header__searchbar-history')
 console.log(SearchbarHistory)
 SearchbarHistory.onmousedown = function(e){
     e.preventDefault();
 }
-=======
-var formValues = [
- { field: 'name', value: 'Sơn Đặng' },
- { field: 'age', value: 18 }
- ]
-function getRequestBodyFromValues(formValues) {
-
-    var newObjects = {}
-    for(var i in formValues ){
-        console.log(formValues[i])
-        var key = formValues[i].field
-        var value = formValues[i].value
-        newObjects[key] = value
-        console.log(newObjects)
-    }
-    
-}
-function getComment (){
-    console.log('123')
-}
-
-
-// getRequestBodyFromValues(formValues)
-// var monsters = [ { name: 'Cá heo', attack: 150, }, { name: 'Khủng long', attack: 150, }, ]
- a= ['1.2', '2', 6, 8]
- inputs= [1, 'hi', false, 8, undefined, '', NaN]
-
-
-console.log( [1, 2, 3, 4].reduce2(function reducer(total, number) { return total + number }))
->>>>>>> 67e1790fb596a4270d1004659136b86f0a860fd6
